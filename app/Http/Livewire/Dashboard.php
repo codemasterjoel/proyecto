1eee78207e61b6bc7303f7f47d17db30387f3d14
codemasterjoel <codemaster19@gmail.com>
2024-06-22 19:11:36 -0400

    grafica integrantes por estado

--- app/Http/Livewire/Dashboard.php
+++ app/Http/Livewire/Dashboard.php
@@ -11,7 +11,7 @@ use DB;
 class Dashboard extends Component
 {
     public $jefe, $integrante = null;
-    public $jefexestado = null;
+    public $jefexestado, $integrantexestado = null;
 
     public function render()
     {
@@ -19,6 +19,7 @@ class Dashboard extends Component
         $this->integrante = Integrante::all();
 
         $this->jefexestado = DB::select('SELECT COUNT(*) as jefes, estados.nombre from registro1x10ffms INNER JOIN estados on registro1x10ffms.estado_id = estados.id GROUP BY estados.nombre');
+        $this->integrantexestado = DB::select('select estados.nombre, count(*) as integrantes from integrantes inner join registro1x10ffms on integrantes.jefe_id = registro1x10ffms.id INNER join estados on registro1x10ffms.estado_id = estados.id GROUP BY estados.nombre');
         return view('livewire.dashboard');
     }
 }
--- resources/views/livewire/dashboard.blade.php
+++ resources/views/livewire/dashboard.blade.php
@@ -95,14 +95,30 @@
           </div>
         </div>
       </div>
+      <div class="row mt-4">
+        <div class="col-lg-12 mb-lg-0 mb-4">
+          <div class="card">
+            <div class="card-body p-3">
+              <h5 class=" font-bold">INTEGRANTES POR ESTADO</h5>
+              <div class="row">
+                <div class="ms-auto text-center mt-5 mt-lg-0">
+                  <div class=" border-radius-lg h-100">
+                    <canvas id="integrantes"></canvas>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   </main>
 
 
 
   <script type="text/javascript">
-  var ctx = document.getElementById('myChart');
-  var myChart = new Chart(ctx, {
+  var ctx = document.getElementById('integrantes');
+  var integrantes = new Chart(ctx, {
       type: 'bar',
       data: {
           labels: [
@@ -146,5 +162,54 @@
           }
       }
   });
+
+  
   </script>
+    <script type="text/javascript">
+      var ctx = document.getElementById('integrantes');
+      var myChart = new Chart(ctx, {
+          type: 'bar',
+          data: {
+              labels: [
+                @foreach ( $integrantexestado as $ixestado )
+                  '{{$ixestado->nombre}}',
+                @endforeach
+              ],
+              datasets: [{
+                  label: '1X10',
+                  data:[
+                    @foreach ( $integrantexestado as $integrante )
+                      '{{$integrante->integrantes}}',
+                    @endforeach
+                  ],
+                  backgroundColor: [
+                      'rgba(255, 99, 132, 0.2)',
+                      'rgba(54, 162, 235, 0.2)',
+                      'rgba(255, 206, 86, 0.2)',
+                      'rgba(75, 192, 192, 0.2)',
+                      'rgba(153, 102, 255, 0.2)',
+                      'rgba(255, 159, 64, 0.2)'
+                  ],
+                  borderColor: [
+                      'rgba(255, 99, 132, 1)',
+                      'rgba(54, 162, 235, 1)',
+                      'rgba(255, 206, 86, 1)',
+                      'rgba(75, 192, 192, 1)',
+                      'rgba(153, 102, 255, 1)',
+                      'rgba(255, 159, 64, 1)'
+                  ],
+                  borderWidth: 1
+              }]
+          },
+          options: {
+              scales: {
+                  yAxes: [{
+                      ticks: {
+                          beginAtZero: true
+                      }
+                  }]
+              }
+          }
+      });
+      </script>
 
